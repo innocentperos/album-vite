eb191f554a2299f779fa893709cc07a0ea87af7b
Innocent Peros <innocentperos@yahoo.com>
2022-10-01 10:42:29 +0100

    Adding components globally Button,Avatar and SInput

--- app.js
+++ app.js
@@ -5,9 +5,17 @@ import { createPinia } from 'pinia'
 import App from './app.vue'
 import router from "./router"
 
+import Avatar from "/src/ui/avatar.vue"
+import Button from "/src/ui/button.vue"
+import SInput from "/src/ui/super-input.vue"
+
 const pinia = createPinia()
 const app = createApp(App)
 
 app.use(pinia)
 app.use(router)
+app.component("Avatar", Avatar)
+.component("Button", Button)
+.component("SInput", SInput)
+
 app.mount('#app')
