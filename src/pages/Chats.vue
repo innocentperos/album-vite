96ca182804a4ea733e59f122fc70690957725d47
Innocent Peros <innocentperos@yahoo.com>
2022-10-02 09:13:16 +0100

    Added unread message bubble to Chats Item

--- src/pages/Chats.vue
+++ src/pages/Chats.vue
@@ -3,21 +3,25 @@
     
     <AppBar back title="Chats">
       <template #actions>
-        <IconButton text class="h-10" icon="bx-user"></IconButton>
+        <IconButton text class="h-10 text-2xl" icon="bx-user"></IconButton>
       </template>
     </AppBar>
     
     <div class="w-full divide-y divide-px divide-slate-600/50">
       
-      <div class="w-full flex px-3 space-x-2 py-4" v-for="i in 16">
+      <div class="w-full flex px-3 py-4 relative" v-for="i in 16">
         <UserAvatar class="mb-auto" small></UserAvatar>
-        <div class="flex flex-col">
+        <div class="ml-2 flex flex-col">
           <span class="font-bold tracking-wider text-lg ">Innocent Peros</span>
           
-          <span class="text-sm tracking-wide ">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, est?<Icon disabled class="inline-flex" icon="bx-check-double text-xl"></Icon>
+          <span class="text-sm tracking-tight ">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, est?
+            <Icon disabled class="inline-flex" icon="bx-check-double text-lg"></Icon>
 
           </span>
+        </div>
+        <div class="text-sm my-auto h-8  aspect-square rounded-full bg-indigo-700 shrink-0 flex items-center justify-center text-white mt-auto">
+          50+
         </div>
       </div>
     </div>
