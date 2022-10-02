ac56abdf52382af6ce7181b3f5057a721580500f
Innocent Peros <innocentperos@yahoo.com>
2022-10-02 16:20:52 +0100

    Added Medium screen support

--- src/pages/Chats.vue
+++ src/pages/Chats.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="w-full min-h-full py-16">
-
+  <div class="md:pl-16 w-full min-h-full flex">
+    
+    <div class="relative w-full divide-y divide-px divide-slate-600/50 md:w-2/5 md:max-h-screen md:overflow-auto">
     <AppBar back title="Chats">
       <template #actions>
         <IconButton text class="h-10 text-2xl" icon="bx-user"></IconButton>
       </template>
     </AppBar>
 
-    <div class="w-full divide-y divide-px divide-slate-600/50">
 
-      <div class="w-full flex px-3 py-4 relative" v-for="i in 16">
+      <div @click="view(i)" class="w-full flex px-3 py-4 relative" v-for="i in 16">
         <UserAvatar  :src="AVATARS.random()" class="mb-auto" small></UserAvatar>
         <div class="ml-2 flex flex-col">
           <span class="font-bold tracking-wider text-lg ">{{randomName()}} {{randomName()}}</span>
@@ -25,14 +25,18 @@
           {{random(50)}}
         </div>
       </div>
-    </div>
-    
-    <div class="w-full p-3 flex items-center justify-center">
+      
+          <div class="w-full p-3 flex items-center justify-center">
       <div class="h-10 aspect-square rounded-full border-2 dark:border-slate-50/50 border-slate-400/50 border-t-indigo-700 dark:border-t-indigo-700 animate-spin">
         
       </div>
     </div>
 
+    </div>
+    
+    <div class="hidden md:flex md:w-3/5 bg-red-500">
+      <span>Nothing to show</span>
+    </div>
     <BottomNavigator v-model="activeTab" fab="bx-plus"></BottomNavigator>
   </div>
 </template>
@@ -41,7 +45,17 @@
   import UserAvatar from "/src/ui/UserAvatar.vue"
   import BottomNavigator from "/src/ui/BottomNavigator.vue"
   import {randomName,random, randomText, AVATARS} from "/stores/usersData"
-
+  import router from "/router"
+  
   import { ref } from "vue"
   const activeTab = ref(3)
+  
+  function view(id){
+    router.push({
+      name:"chat",
+      params:{
+        id:id
+      }
+    })
+  }
 </script>
--- src/ui/BottomNavigator.vue
+++ src/ui/BottomNavigator.vue
@@ -1,11 +1,11 @@
 <template>
-  <IconButton v-if="fab" :loading="fabLoading" @click="fabClick" class="fixed mb-3 bottom-16 right-0 mr-3 z-10 w-16 h-16 aspect-square shadow-xl rounded-full">
+  <IconButton v-if="fab" :loading="fabLoading" @click="fabClick" class="fixed mb-3 bottom-16 md:bottom-3 right-0 mr-3 z-10 w-16 h-16 aspect-square shadow-xl rounded-[50%]">
     <Icon strip :icon="fab + ' text-white text-2xl'"></Icon>
   </IconButton>
   
-  <div class="fixed bottom-0 left-0 right-0 w-full h-16 bg-slate-100 dark:bg-slate-800 dark:shadow-[4px_1px_60px_-1px_rgba(0,0,0,0.43)] shadow-[4px_1px_60px_-1px_rgba(229,231,235,.93)]  flex items-center justify-between px-3 p-1">
+  <div class="fixed bottom-0 left-0 right-0 w-full h-16 bg-slate-100 dark:bg-slate-800 dark:shadow-[4px_1px_60px_-1px_rgba(0,0,0,0.43)] shadow-[4px_1px_60px_-1px_rgba(229,231,235,.93)]  flex items-center justify-between px-3 p-1 md:flex-col md:w-16 md:h-full md:justify-start md:z-20 md:shadow-none md:border md:border-transparent md:border-r-slate-600 ">
 
-    <div v-for="icon,index in actions" :key="index" class=" h-full aspect-square flex items-center justify-center text-white rounded-xl duration-300" :class="bgStyle(index)">
+    <div v-for="icon,index in actions" :key="index" class=" h-full md:w-14 md:h-auto aspect-square flex items-center justify-center text-white rounded-xl duration-300 last:mt-auto" :class="bgStyle(index)">
       <IconButton @click="change(index)" :icon="icon + ' text-2xl hover:text-white'" text>
       </IconButton>
 
@@ -34,7 +34,8 @@
     "bx-home",
     "bx-search",
     "bx-sun",
-    "bx-message"
+    "bx-message",
+    "bx-power-off"
     ]
 
   const selected = computed(function(){
